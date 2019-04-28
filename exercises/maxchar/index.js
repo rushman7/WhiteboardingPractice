713a097d91de7dfe75281030867583301d7c6496
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-04-28 17:19:42 -0400

    maxchar second solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -12,7 +12,26 @@ function maxChar(str) {
     chars[char] = chars[char] + 1 || 1;
   }
 
-  return Object.keys(chars).reduce((a, b) => chars[a] > chars[b] ? a : b)
+  return Object.keys(chars).reduce((a, b) => chars[a] > chars[b] ? a : b);
 }
+
+// function maxChar(str) {
+//   let chars = {};
+//   let max = 0;
+//   let maxChar = '';
+
+//   for (let char of str) {
+//     chars[char] = chars[char] + 1 || 1;
+//   }
+
+//   for (let char in chars) {
+//     if (chars[char] > max) {
+//       max = chars[char];
+//       maxChar = char
+//     }
+//   }
+
+//   return maxChar;
+// }
 
 module.exports = maxChar;
