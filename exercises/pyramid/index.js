d4ffd6c293d3ac470321fbb0faaf0a2df72f48ee
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-06-21 17:03:31 -0400

    pyramid solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,20 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  for (let i = 0; i < n; i++) {
+    let level = '';
+
+    for (let j = 0; j < n * 2 - 1 ;j++) {
+      if (midpoint - i <= j && midpoint + i >= j) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
