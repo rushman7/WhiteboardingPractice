1ea9dd64d42a88c34826f937ee9e41c1199d2287
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-06-21 17:16:46 -0400

    recusrive solution for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,20 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2);
-  for (let i = 0; i < n; i++) {
-    let level = '';
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let i = 0; i < n; i++) {
+//     let level = '';
 
-    for (let j = 0; j < n * 2 - 1 ;j++) {
-      if (midpoint - i <= j && midpoint + i >= j) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
-    console.log(level);
+//     for (let j = 0; j < n * 2 - 1 ;j++) {
+//       if (midpoint - i <= j && midpoint + i >= j) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+function pyramid(n, i = 0, level = '') {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  if (n === i) {
+    return;
   }
+
+  if (n * 2 - 1 === level.length) {
+    console.log(level);
+    return pyramid(n, i + 1);
+  }
+
+  if (midpoint - i <= level.length && midpoint + i >= level.length) {
+    level += '#';
+  } else {
+    level += ' ';
+  }
+  pyramid(n, i, level);
 }
 
 module.exports = pyramid;
