2d80a2247904cf919414d0d2f8eb9faca5d32cdd
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-06-25 19:25:56 -0400

    memoize solution for fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -34,12 +34,28 @@
 //   return result[n];
 // }
 
-function fib(n) { // recursive solution
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+function recurFib(n) { // recursive solution
   if (n < 2) {
     return n;
   }
 
   return fib(n - 1) + fib(n - 2);
-}
+};
+
+const fib = memoize(recurFib);
 
 module.exports = fib;
