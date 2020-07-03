b27e6834dfce6258f6eb1cf5881b1910b5c9c8d6
BuildTools <arthurpisakhov1@gmail.com>
2020-07-03 18:10:45 -0400

    string comp easy solution

---
+++ LeetCode/String-Compression.js
@@ -0,0 +1,75 @@
+// Given an array of characters, compress it in-place.
+
+// The length after compression must always be smaller than or equal to the original array.
+
+// Every element of the array should be a character (not int) of length 1.
+
+// After you are done modifying the input array in-place, return the new length of the array.
+
+ 
+// Follow up:
+// Could you solve it using only O(1) extra space?
+
+ 
+// Example 1:
+
+// Input:
+// ["a","a","b","b","c","c","c"]
+
+// Output:
+// Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+
+// Explanation:
+// "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+ 
+
+// Example 2:
+
+// Input:
+// ["a"]
+
+// Output:
+// Return 1, and the first 1 characters of the input array should be: ["a"]
+
+// Explanation:
+// Nothing is replaced.
+ 
+
+// Example 3:
+
+// Input:
+// ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+
+// Output:
+// Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+
+// Explanation:
+// Since the character "a" does not repeat, it is not compressed. "bbbbbbbbbbbb" is replaced by "b12".
+// Notice each digit has it's own entry in the array.
+
+var compress = function(chars) {
+  if (chars.length < 2) return chars.length;
+  let val = 0;
+  let curr = chars[0];
+  
+  for (let i=0;i<chars.length;i++) {
+      val++
+      if (chars[i+1] != curr) {
+          chars.splice(i-(val-2),val-2)
+          if (val > 1) {
+              let newVal = val.toString()
+              if (newVal.length > 1) {
+                  chars.splice(i-(val-2), 0, newVal[0].toString())
+                  chars[i-(val-3)] = newVal[1].toString()
+                  i = i-(val-2)
+              } else {
+                  chars[i-(val-2)] = newVal.toString()
+                  i = i-(val-2)
+              }
+          }
+          curr = chars[i+1]
+          val = 0
+      }
+  }
+  return chars.length
+};
