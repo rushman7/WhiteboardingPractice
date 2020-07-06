52d42714d961b8ffd6a6b026e0144cd4ca84e317
BuildTools <arthurpisakhov1@gmail.com>
2020-07-05 20:53:43 -0400

    rotate image medium

---
+++ LeetCode/Rotate-Image.js
@@ -0,0 +1,69 @@
+// You are given an n x n 2D matrix representing an image.
+
+// Rotate the image by 90 degrees (clockwise).
+
+// Note:
+
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+// Example 1:
+
+// Given input matrix = 
+// [
+//   [1,2,3],
+//   [4,5,6],
+//   [7,8,9]
+// ],
+
+// rotate the input matrix in-place such that it becomes:
+// [
+//   [7,4,1],
+//   [8,5,2],
+//   [9,6,3]
+// ]
+// Example 2:
+
+// Given input matrix =
+// [
+//   [ 5, 1, 9,11],
+//   [ 2, 4, 8,10],
+//   [13, 3, 6, 7],
+//   [15,14,12,16]
+// ], 
+
+// rotate the input matrix in-place such that it becomes:
+// [
+//   [15,13, 2, 5],
+//   [14, 3, 4, 1],
+//   [12, 6, 8, 9],
+//   [16, 7,10,11]
+// ]
+
+var rotate = function(matrix) {
+  let n = matrix.length;
+  if (n <= 1) return matrix;
+  
+  for (row in matrix) {
+      for (let col=row;col<n;col++) {
+          if (col === row) continue;
+          let temp = matrix[row][col];
+          matrix[row][col] = matrix[col][row];
+          matrix[col][row] = temp;
+      }
+  }
+  
+  for (row in matrix) {
+      let t1 = 0;
+      let t2 = n-1;
+      
+      for (let col=0;col<n/2;col++) {
+          let temp = matrix[row][t1];
+          matrix[row][t1] = matrix[row][t2];
+          matrix[row][t2] = temp
+          t1++;
+          t2--;
+      }
+  }
+  
+  return matrix
+};
