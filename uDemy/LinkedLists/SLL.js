905c0c040154c42addafd7e00a1b274e3fc811fb
BuildTools <arthurpisakhov1@gmail.com>
2020-10-10 20:20:44 -0400

    set, insert, remove

--- uDemy/LinkedLists/SLL.js
+++ uDemy/LinkedLists/SLL.js
@@ -48,6 +48,7 @@ class SinglyLinkedList{
     if (!this.length) this.head = this.tail = null;
     return this;
   }
+
   unshift(val) {
     let node = new Node(val);
     if (!this.length) this.head = this.tail = node
@@ -59,16 +60,49 @@ class SinglyLinkedList{
     this.length++
     return this;
   }
-  get(num) {
+
+  get(index) {
+    if (index < 0 || index > this.length) return null;
     let i = 0;
     let curr = this.head;
 
-    while (i < num && curr) {
+    while (i < index) {
       i++
       curr = curr.next
     }
 
-    return curr ? curr : 'Does not exist.';
+    return curr;
+  }
+
+  set(index, val) {
+    if (index < 0 || index > this.length) return null;
+    let node = this.get(index);
+    node.val = val;
+    return node;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return null;
+    if (index === this.length-1) return this.push(val);
+    if (index === 0) return this.unshift(val);
+    let node = new Node(val);
+    let curr = this.get(index-1)
+ 
+    node.next = curr.next;
+    curr.next = node;
+    this.length++
+    return node;
+  }
+
+  remove(index) { 
+    if (index < 0 || index > this.length) return null;
+    if (index === this.length-1) return this.pop();
+    if (index === 0) return this.shift();
+
+    let prev = this.get(index-1);
+    prev.next = prev.next.next;
+    this.length--
+    return this;
   }
 }
 
@@ -77,10 +111,12 @@ const SLL = new SinglyLinkedList();
 SLL.push(5)
 SLL.push(10)
 SLL.push(15)
-SLL.unshift(20)
+SLL.unshift(20) // 20 -> 5 -> 10 -> 15
+// console.log(SLL.insert(2, 8)) // 20 -> 5 -> 8 -> 10 -> 15
+SLL.remove(3) // 20 -> 5 -> 15
+// SLL.set(1, 8) // 5 --> 8
+// console.log(SLL.get(1))
 // SLL.push(20)
 // SLL.pop()
 // SLL.shift()
-console.log(SLL)
-console.log(SLL.get(10))
-console.log(SLL.get(1))
+console.log(SLL) // 20 -> 5 -> 10 -> 15
