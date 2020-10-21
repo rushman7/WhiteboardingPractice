d767a9c12b37cdd69ff680b4da4dd6c4eac66b65
BuildTools <arthurpisakhov1@gmail.com>
2020-10-21 15:39:34 -0400

    Max Binary Heap removeMax

--- uDemy/Trees/MaxBinaryHeap.js
+++ uDemy/Trees/MaxBinaryHeap.js
@@ -15,34 +15,61 @@ class MaxBinaryHeap {
       parentIndex = Math.floor((index-1)/2);
     }
   }
+
+  removeMax() {
+    if (this.values.length < 2) return this.values = [];
+    [this.values[0], this.values[this.values.length-1]] = [this.values[this.values.length-1], this.values[0]]
+    this.values.pop();
+    let curr = 0,
+        left = (2*curr) + 1,
+        right = (2*curr) + 2
+
+    while (this.values[curr] < this.values[left] || this.values[curr] < this.values[right]) {
+      if (left < this.values.length && right < this.values.length) {
+        if (this.values[left] > this.values[right]) {
+          [this.values[curr], this.values[left]] = [this.values[left], this.values[curr]];
+          curr = left;
+          left = (2*curr) + 1;
+          right = (2*curr) + 2;
+        } else {
+          [this.values[curr],this.values[right]] = [this.values[right], this.values[curr]];
+          curr = right;
+          left = (2*curr) + 1;
+          right = (2*curr) + 2;
+        }
+      } else if (left < this.values.length) {
+        [this.values[curr], this.values[left]] = [this.values[left], this.values[curr]];
+        break;
+      }
+    }
+    
+    return console.log(this.values)
+  }
+
+
 }
 
 const maxBiHeap = new MaxBinaryHeap();
 
-maxBiHeap.insert(6)
-maxBiHeap.insert(15)
 maxBiHeap.insert(20)
-maxBiHeap.insert(3)
-maxBiHeap.insert(8)
-maxBiHeap.insert(13)
+maxBiHeap.insert(25)
 maxBiHeap.insert(17)
 maxBiHeap.insert(27)
-maxBiHeap.insert(1)
 maxBiHeap.insert(87)
-maxBiHeap.insert(14)
 maxBiHeap.insert(28)
-maxBiHeap.insert(4)
-maxBiHeap.insert(9)
-maxBiHeap.insert(25)
-console.log(maxBiHeap.values);
+maxBiHeap.insert(8)
+maxBiHeap.removeMax()
+// console.log(maxBiHeap.values);
 
 /*
               87
         27          28
-      8   20     17    25
-    3  1 6  14 13  4  9  15
+      25   20     17    8
 
+              27
+        25          17
+      20   8       
 
-    [87, 27, 28,  8, 20, 17, 25,  3,  1,  6, 14, 13,  4,  9, 15]
-    [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14]
+    [87, 27, 28,  8, 20, 17, 25]
+    [ 0,  1,  2,  3,  4,  5,  6]
 */
