56759c421594c4e21637826ca4cfa96fe6e909c2
BuildTools <arthurpisakhov1@gmail.com>
2020-10-21 22:19:35 -0400

    optimized hash function

--- uDemy/HashTables/HashTable.js
+++ uDemy/HashTables/HashTable.js
@@ -1,23 +1,17 @@
+String.prototype.hashCode = function(size) {
+  let hash = 0;
+  if (this.length == 0) return hash;
+  for (let i = 0; i < this.length; i++) hash = (((hash<<5)-hash) + this.charCodeAt(i)) % size;
+  return hash;
+}
+
 class HashTable {
   constructor(size=13) {
     this.keyMap = new Array(size);
   }
 
-  _hash(key, arrayLen) {
-    let total = 0;
-    let prime = 31;
-  
-    for (let i = 0; i < Math.min(key.length, 100);i++) {
-      let char = key[i];
-      let value = char.charCodeAt(0);
-      total = (total * prime + value) % arrayLen;
-    }
-  
-    return total;
-  }
-
   set(key, value) {
-    let idx = this._hash(key, this.keyMap.length)
+    let idx = key.hashCode(this.keyMap.length)
     if (this.keyMap[idx]) this.keyMap[idx].push([key, value])
     else {
       let arr = [[key, value]];
@@ -28,7 +22,7 @@ class HashTable {
   }
 
   get(key) {
-    let idx = this._hash(key, this.keyMap.length)
+    let idx = key.hashCode(this.keyMap.length)
     if (this.keyMap[idx]) {
       if (this.keyMap[idx].length > 1) {
         for (let i=0;i<this.keyMap[idx].length;i++) {
@@ -75,13 +69,12 @@ const ht = new HashTable();
 
 ht.set('blue', '#585AEE')
 ht.set('black', '#000000')
-ht.set('black', '#000000')
 ht.set('orange', '#DD9E36')
 ht.set('green', '#48DD36')
 ht.set('purple', '#865CE0')
 ht.set('yellow', '#D2E05C')
 ht.set('red', '#F33636')
 
-// console.log(ht.keyMap);
-console.log(ht.keys())
-console.log(ht.values())
+console.log(ht.keyMap);
+// console.log(ht.keys())
+// console.log(ht.values())
