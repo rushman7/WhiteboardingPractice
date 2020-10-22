766ef74658ef882655c38f9f74b5c223896a3ca8
BuildTools <arthurpisakhov1@gmail.com>
2020-10-21 23:16:50 -0400

    removeVertex

--- uDemy/Graphs/Graph.js
+++ uDemy/Graphs/Graph.js
@@ -16,6 +16,13 @@ class Graph {
     if (this.adj[v1]) this.adj[v1] = this.adj[v1].filter(a => a != v2);
     if (this.adj[v2]) this.adj[v2] = this.adj[v2].filter(a => a != v1);
   }
+
+  removeVertex(data) {
+    for (let vertex in this.adj) {
+      this.removeEdge(data, vertex);
+    }
+    delete this.adj[data];
+  }
 }
 
 const graph = new Graph();
@@ -24,10 +31,16 @@ graph.addVertex('Tokyo');
 graph.addVertex('London');
 graph.addVertex('California');
 graph.addVertex('China');
+graph.addVertex('Florida');
+graph.addVertex('Rome');
 graph.addEdge('Tokyo','China');
+graph.addEdge('California','London');
+graph.addEdge('Rome','London');
+graph.addEdge('California','Florida');
 graph.addEdge('Tokyo','London');
 graph.addEdge('China','California');
-graph.removeEdge('Tokyo','China');
+// graph.removeEdge('Tokyo','China');
+// graph.removeVertex('China')
 
 
 console.log(graph);
