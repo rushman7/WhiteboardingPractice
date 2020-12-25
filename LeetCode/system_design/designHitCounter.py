37b895f5f9818ef8a141a453222e9f2c2683d40e
BuildTools <arthurpisakhov1@gmail.com>
2020-12-25 14:22:13 -0500

    design hit counter, medium

---
+++ LeetCode/system_design/designHitCounter.py
@@ -0,0 +1,49 @@
+# Design a hit counter which counts the number of hits received in the past 5 minutes.
+
+# Each function accepts a timestamp parameter (in seconds granularity) and you may assume that calls are being made to the system in chronological order (ie, the timestamp is monotonically increasing). You may assume that the earliest timestamp starts at 1.
+
+# It is possible that several hits arrive roughly at the same time.
+
+# Example:
+
+# HitCounter counter = new HitCounter();
+
+# // hit at timestamp 1.
+# counter.hit(1);
+
+# // hit at timestamp 2.
+# counter.hit(2);
+
+# // hit at timestamp 3.
+# counter.hit(3);
+
+# // get hits at timestamp 4, should return 3.
+# counter.getHits(4);
+
+# // hit at timestamp 300.
+# counter.hit(300);
+
+# // get hits at timestamp 300, should return 4.
+# counter.getHits(300);
+
+# // get hits at timestamp 301, should return 3.
+# counter.getHits(301); 
+# Follow up:
+# What if the number of hits per second could be very large? Does your design scale?
+class HitCounter:
+
+    def __init__(self):
+        self.q = []
+        
+
+    def hit(self, timestamp: int) -> None:
+        self.q.insert(0, timestamp)
+        while self.q[0] >= self.q[-1]+300:
+            self.q.pop()
+        
+
+    def getHits(self, timestamp: int) -> int:
+        while len(self.q) and self.q[-1]+300 <= timestamp:
+            self.q.pop()
+
+        return len(self.q)
