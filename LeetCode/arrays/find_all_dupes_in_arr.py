5c946939d7f5f93f382101c5092e6c857be79bd7
BuildTools <arthurpisakhov1@gmail.com>
2021-01-11 14:33:14 -0500

    442. Find All Duplicates in an Array [Medium]

---
+++ LeetCode/arrays/find_all_dupes_in_arr.py
@@ -0,0 +1,23 @@
+# Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+# Find all the elements that appear twice in this array.
+
+# Could you do it without extra space and in O(n) runtime?
+
+# Example:
+# Input:
+# [4,3,2,7,8,2,3,1]
+
+# Output:
+# [2,3]
+
+def findDuplicates(self, nums: List[int]) -> List[int]:
+    dupes = []
+    
+    for num in nums:
+        if nums[abs(num)-1] < 0:
+            dupes.append(abs(num))
+        else:
+            nums[abs(num)-1]*=-1
+    
+    return dupes
