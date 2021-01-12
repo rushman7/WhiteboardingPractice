a519b92d4dfb42059487ab6517ca20c42582d5ba
BuildTools <arthurpisakhov1@gmail.com>
2021-01-12 12:13:06 -0500

    2. Add Two Numbers [Medium]

--- LeetCode/linked_lists/Add-Two-Numbers.py
+++ LeetCode/linked_lists/Add-Two-Numbers.py
@@ -45,3 +45,52 @@ class Solution:
         sentinal.next = move_carry(sentinal.next)
         
         return sentinal.next
+
+# O(1) extra space solution
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        curr1, curr2 = l1, l2
+        len1, len2 = self.find_len(l1), self.find_len(l2)
+        list1 = True
+        if len2 > len1:list1 = False
+        
+        while curr1 or curr2:
+            if list1:
+                if curr1 and curr2:
+                    curr1.val+=curr2.val
+                    curr1, curr2 = curr1.next, curr2.next
+                else:
+                    curr1 = curr1.next
+            else:
+                if curr1 and curr2:
+                    curr2.val+=curr1.val
+                    curr1, curr2 = curr1.next, curr2.next
+                else:
+                    curr2 = curr2.next
+        if list1:
+            l1 = self.move_carry(l1)
+        else:
+            l2 = self.move_carry(l2)
+        
+        return l1 if list1 else l2
+    
+    def find_len(self, node, count=0):
+        if not node: return count
+        return self.find_len(node.next, count+1)
+    
+    def move_carry(self, head):
+        curr = head
+        carry = 0
+
+        while curr:
+            curr.val+=carry
+            carry = 0
+            if curr.val >= 10:
+                carry = 1
+                curr.val-=10
+            if carry and not curr.next:
+                curr.next = ListNode(carry)
+                break
+            curr = curr.next
+
+        return head
