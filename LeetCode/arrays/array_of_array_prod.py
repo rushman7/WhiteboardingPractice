e517ee6c5a2c5f1cb517c4afbff6a9415ab0f335
BuildTools <arthurpisakhov1@gmail.com>
2021-01-26 18:19:22 -0500

    cleaner solution

--- LeetCode/arrays/array_of_array_prod.py
+++ LeetCode/arrays/array_of_array_prod.py
@@ -1,19 +1,19 @@
-def array_of_array_products(arr):
-  if len(arr) <= 1:
-    return []
-  if len(arr) == 2:
-    arr[0], arr[1] = arr[1], arr[0]
-    return arr
-  L = [1] * len(arr)
-  R = [1] * len(arr)
-  ans = [1] * len(arr)
-
-  for i in range(1, len(arr)):
-    L[i] = L[i-1]*arr[i-1]
-  for i in range(len(arr)-2, -1,-1):
-    R[i] = R[i+1]*arr[i+1]
+def arrayOfProducts(array):
+    if len(array) <= 1:
+      return []
+    if len(array) == 2:
+      array[0], array[1] = array[1], array[0]
+      return array
+    length = len(array)
+    left_array = [1] * length
+    right_array = [1] * length
+    result = [1] * length
     
-  for i in range(len(ans)):
-    ans[i] = R[i]*L[i]
-   
-  return ans
+    for i in range(1, length):
+      left_array[i] = left_array[i-1]*array[i-1]
+    for j in range(length-2, -1, -1):
+      right_array[j] = right_array[j+1]*array[j+1]
+    for k in range(length):
+      result[k] = right_array[k]*left_array[k]
+    
+    return result
