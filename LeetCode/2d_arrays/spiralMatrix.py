43c7ecf9b06d3caaff174663068785dec3b3a93f
BuildTools <arthurpisakhov1@gmail.com>
2021-01-28 16:12:03 -0500

    fixed spiral

--- LeetCode/2d_arrays/spiralMatrix.py
+++ LeetCode/2d_arrays/spiralMatrix.py
@@ -13,26 +13,32 @@
 # Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 # Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
-def spiralTraverse(array):
-	startRow = startCol = 0
-	endRow, endCol = len(array)-1, len(array[0])-1
-	ans = []
-	while startRow <= endRow and startCol <= endCol:
-		for col in range(startCol, endCol+1):
-			ans.append(array[startRow][col])
-		startRow+=1
-		for row in range(startRow, endRow+1):
-			ans.append(array[row][endCol])
-		endCol-=1
-		for col in reversed(range(startCol, endCol+1)):
-			if startRow > endRow:
-				break
-			ans.append(array[endRow][col])
-		endRow-=1 
-		for row in reversed(range(startRow, endRow+1)):
-			if startCol > endCol:
-				break
-			ans.append(array[row][startCol])
-		startCol+=1
+def spiral_copy(inputMatrix):
+  TR, BR, = 0, len(inputMatrix)-1
+  LC, RC = 0, len(inputMatrix[0])-1
+  ans = []
+  while TR <= BR and LC <= RC:
+      for col in range(LC, RC):
+        ans.append(inputMatrix[TR][col])
 
-	return ans
+      for row in range(TR, BR):
+        ans.append(inputMatrix[row][RC])
+
+      if TR == BR:
+        ans.append(inputMatrix[TR][RC])
+        BR+=1
+        TR-=1
+        break
+
+      for col in range(RC, LC, -1):
+        ans.append(inputMatrix[BR][col])
+
+      for row in range(BR, TR, -1):
+        ans.append(inputMatrix[row][LC])
+
+      LC+=1
+      RC-=1
+      TR+=1
+      BR-=1
+
+  return ans
