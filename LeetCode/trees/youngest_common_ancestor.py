5a96d7f1ea03d35e945b5763df04019a634d226a
BuildTools <arthurpisakhov1@gmail.com>
2021-02-01 17:50:30 -0500

    Youngest Common Ancestor [Medium] [AlgoExpert]

--- LeetCode/trees/youngest_common_ancestor.py
+++ LeetCode/trees/youngest_common_ancestor.py
@@ -8,24 +8,24 @@ def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
     descendantOneDepth = getDepths(topAncestor, descendantOne)
     descendantTwoDepth = getDepths(topAncestor, descendantTwo)
 
-	while descendantOneDepth != descendantTwoDepth:
-		if descendantOneDepth > descendantTwoDepth:
-			descendantOne = descendantOne.ancestor
-			descendantOneDepth-=1
-		else:
-			descendantTwo = descendantTwo.ancestor
-			descendantTwoDepth-=1
-		if descendantOne == descendantTwo:
-			return descendantOne
-	
-	while descendantOne.ancestor != descendantTwo.ancestor:
-		descendantTwo = descendantTwo.ancestor
-		descendantOne = descendantOne.ancestor
-	
-	return descendantOne.ancestor
+    while descendantOneDepth != descendantTwoDepth:
+      if descendantOneDepth > descendantTwoDepth:
+        descendantOne = descendantOne.ancestor
+        descendantOneDepth-=1
+      else:
+        descendantTwo = descendantTwo.ancestor
+        descendantTwoDepth-=1
+      if descendantOne == descendantTwo:
+        return descendantOne
+    
+    while descendantOne.ancestor != descendantTwo.ancestor:
+      descendantTwo = descendantTwo.ancestor
+      descendantOne = descendantOne.ancestor
+    
+    return descendantOne.ancestor
 
 def getDepths(node, val, depth=0):
-	while node.name != val.name:
-		val = val.ancestor
-		depth+=1
-	return depth
+    while node.name != val.name:
+      val = val.ancestor
+      depth+=1
+    return depth
