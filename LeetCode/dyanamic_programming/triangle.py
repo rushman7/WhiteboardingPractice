9c7d8bd1f968d2e9f3621b23ef2c1eb859d06ac2
BuildTools <arthurpisakhov1@gmail.com>
2021-04-21 13:11:03 -0400

    120. Triangle [Medium] [LeetCode]

---
+++ LeetCode/dyanamic_programming/triangle.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for i in range(len(triangle)-1, 0, -1):
+            for j in range(len(triangle[i])-1):
+                triangle[i-1][j] += min(triangle[i][j], triangle[i][j+1])
+        
+        return triangle[0][0]
